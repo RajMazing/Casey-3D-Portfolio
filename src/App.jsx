9264bc55ff2b54d0e14751cbfe397ec58df8bdd1
Casey <caseyraj92@gmail.com>
2025-06-09 15:47:40 -0500

    Added sections and will separate each sections ffor test and readability

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,21 @@ import Contact from "./components/contact/Contact";
 const App = () => {
   return (
     <div className="container">
-      <Hero />
-      <Services />
-      <Portfolio />
-      <Contact />
+      <section id="#home">
+        {" "}
+        <Hero />{" "}
+      </section>
+      <section id="#services">
+        <Services />
+      </section>
+      <section id="#portfolio">
+        {" "}
+        <Portfolio />
+      </section>
+      <section id="#contact">
+        {" "}
+        <Contact />
+      </section>
     </div>
   );
 };
